e69a673033800815eed1464f79777107fbb8122d
HanuYat <jnjn1235@gmail.com>
2026-03-12 18:03:52 +0900

    익명 메서드 (3차)

--- CounterFactory/CounterFactory.cs
+++ CounterFactory/CounterFactory.cs
@@ -4,34 +4,28 @@ class CounterFactory
 {
     private static int _count = 0;
 
-    public static Func<int> CreateSimpleCounter()
+    public static Func<int> CreateSimpleCounter = () =>
     {
-        return () =>
-        {
-            _count++;
-            return _count;
-        };
-    }
+        int count = _count++;        
+        return count;
+    };
 
-    public static Func<int> CreateStepCounter(int step)
+    public static Func<int, int> CreateStepCounter = (int step) =>
     {
-        return () =>
-        {
-            _count += step;
-            return _count;
-        };
-    }
+        _count += step;
+        return _count;
+    };
 
-    public static Func<int> CreateBoundedCounter(int min, int max)
-    {
-        return () =>
-        {
-            for (int i = min; i <= max; i++)
-            {              
-                return _count++;
-            }
-        };
-    }
+    //public static Func<int> CreateBoundedCounter(int min, int max)
+    //{
+    //    return () =>
+    //    {
+    //        for (int i = min; i <= max; i++)
+    //        {              
+    //            return _count++;
+    //        }
+    //    };
+    //}
 
     public static void CreateResettableCounter()
     {
--- CounterFactory/Program.cs
+++ CounterFactory/Program.cs
@@ -3,3 +3,9 @@ using System;
 // README.md를 읽고 아래에 코드를 작성하세요.
 
 Console.WriteLine("=== 단순 카운터 ===");
+var count1 = CounterFactory.CreateSimpleCounter();
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"{count1} ");
+}
